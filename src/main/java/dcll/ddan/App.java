f721e7f017127d157d8cc12da28c69d05044816f
lioz3190 <ddavid3190@gmail.com>
2016-02-17 11:57:30 +0100

    initial commit

---
+++ src/main/java/dcll/ddan/App.java
@@ -0,0 +1,13 @@
+package dcll.ddan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

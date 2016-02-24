481039afcdaac3aec19363f40bc01b5815f40114
lioz3190 <ddavid3190@gmail.com>
2016-02-24 10:45:55 +0100

    commit implementation SimpleStack

---
+++ src/main/java/dcll/ddan/MySimpleStack/SimpleStackImp.java
@@ -0,0 +1,37 @@
+package dcll.ddan.MySimpleStack;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ * Created by Lioz-MBPR on 24/02/16.
+ */
+public class SimpleStackImp implements SimpleStack{
+
+    Stack<Object> wrappedStack = new Stack<Object>();
+
+    @Override
+    public boolean isEmpty() {
+        return wrappedStack.isEmpty();
+    }
+
+    @Override
+    public int getSize() {
+        return wrappedStack.size();
+    }
+
+    @Override
+    public void push(Item item) {
+        wrappedStack.push(item);
+    }
+
+    @Override
+    public Item peek() throws EmptyStackException {
+        return (Item) wrappedStack.peek();
+    }
+
+    @Override
+    public Item pop() throws EmptyStackException {
+        return (Item) wrappedStack.pop();
+    }
+}
